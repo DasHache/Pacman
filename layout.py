d490aa6dfacc91529a2d3127f446d92118f8bd23
kowaraj <kowaraj4stuff@gmail.com>
2016-07-05 22:36:50 +0200

    changed var's name for lines

--- layout.py
+++ layout.py
@@ -4,9 +4,9 @@ class Layout :
 
     def __init__(self,filename):
         f = open(filename ,'r')
-        l = f.readlines()
-        self.height = len(l)
-        self.width = len(l[0])
+        lines = f.readlines()
+        self.height = len(lines)
+        self.width = len(lines[0])
 
         self.walls = [[0,0],[0,1],[0,3],[1,0],[2,0],[3,3]]
         print(self.walls[0])
